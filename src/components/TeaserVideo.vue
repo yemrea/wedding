faa3d05206a474f0efe2d1b479cf23b63e6cd0bf
Yunus Emre Alpozen <yemrea@microsoft.com>
2023-01-16 16:04:34 +0100

    Moving to new teaser video

--- src/components/TeaserVideo.vue
+++ src/components/TeaserVideo.vue
@@ -4,7 +4,7 @@
         <div class="container">
             <div class="video-wrap">
                 <img src="@/assets/images/teaser/thumbnail.jpg" alt="">
-                <a href="https://www.youtube.com/embed/29Es4e_dYA8" class="video-btn" data-type="iframe"><i
+                <a href="https://www.youtube.com/embed/IFcG4AQtkl4" class="video-btn" data-type="iframe"><i
                         class="fi flaticon-play"></i></a>
             </div>
         </div>
