3efcb445c0f7b0aafb66253c8858779cd5d98cd7
Yunus Emre Alpozen <yemrea@microsoft.com>
2023-01-16 15:00:44 +0100

    change video player to fix loading issue

--- src/components/TeaserVideo.vue
+++ src/components/TeaserVideo.vue
@@ -2,10 +2,17 @@
     <!-- start wpo-video-section -->
     <section class="wpo-video-section-s2 pb-0 section-padding">
         <div class="container">
-            <div class="video-wrap">
-                <img src="@/assets/images/teaser/thumbnail.jpg" alt="">
-                <a href="https://www.youtube.com/embed/29Es4e_dYA8" class="video-btn" data-type="iframe"><i
-                        class="fi flaticon-play"></i></a>
+            <div class="row">
+                <div class="col-1 d-block col-md-1 d-sm-none d-xs-none"> </div>
+                <div class="col-10 d-block col-sm-12 col-xs-12">
+                    <div class="video-wrap">
+                        <iframe width="100%" height="315"
+                            src="https://www.youtube.com/embed/29Es4e_dYA8" title="YouTube video player" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div class="col-1 d-block col-md-1 d-sm-none d-xs-none"> </div>
             </div>
         </div>
     </section>
