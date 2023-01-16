d69aadfdffe9617b7f13c20f3ee7aa04a09cd226
Yunus Emre Alpozen <yemrea@microsoft.com>
2023-01-16 15:25:08 +0100

    Test iphone video player issue

--- src/components/TeaserVideo.vue
+++ src/components/TeaserVideo.vue
@@ -2,17 +2,10 @@
     <!-- start wpo-video-section -->
     <section class="wpo-video-section-s2 pb-0 section-padding">
         <div class="container">
-            <div class="row">
-                <div class="col-1 d-block col-md-1 d-sm-none d-xs-none"> </div>
-                <div class="col-10 d-block col-sm-12 col-xs-12">
-                    <div class="video-wrap">
-                        <iframe width="100%" height="315"
-                            src="https://www.youtube.com/embed/29Es4e_dYA8" title="YouTube video player" frameborder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            allowfullscreen></iframe>
-                    </div>
-                </div>
-                <div class="col-1 d-block col-md-1 d-sm-none d-xs-none"> </div>
+            <div class="video-wrap">
+                <img src="@/assets/images/teaser/thumbnail.jpg" alt="">
+                <a href="https://www.youtube.com/embed/OUB6zKc-Tos" class="video-btn" data-type="iframe"><i
+                        class="fi flaticon-play"></i></a>
             </div>
         </div>
     </section>
